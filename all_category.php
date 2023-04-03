a3a144a8d2c8fdbcec30df48d76118d499344864
Solomon Akinola <s.akinola@rgu.ac.uk>
2023-04-03 15:23:52 +0100

    updates

--- all_category.php
+++ all_category.php
@@ -43,26 +43,10 @@ include 'components/like_stories.php';
    <h1 class="heading">post categories</h1>
 
    <div class="box-container">
-      <div class="box"><span>01</span><a href="category.php?category=nature">nature</a></div>
-      <div class="box"><span>02</span><a href="category.php?category=eduction">education</a></div>
-      <div class="box"><span>03</span><a href="category.php?category=pets and animals">pets and animals</a></div>
-      <div class="box"><span>04</span><a href="category.php?category=technology">technology</a></div>
-      <div class="box"><span>05</span><a href="category.php?category=fashion">fashion</a></div>
-      <div class="box"><span>06</span><a href="category.php?category=entertainment">entertainment</a></div>
-      <div class="box"><span>07</span><a href="category.php?category=movies">movies</a></div>
-      <div class="box"><span>08</span><a href="category.php?category=gaming">gaming</a></div>
-      <div class="box"><span>09</span><a href="category.php?category=music">music</a></div>
-      <div class="box"><span>10</span><a href="category.php?category=sports">sports</a></div>
-      <div class="box"><span>11</span><a href="category.php?category=news">news</a></div>
-      <div class="box"><span>12</span><a href="category.php?category=travel">travel</a></div>
-      <div class="box"><span>13</span><a href="category.php?category=comedy">comedy</a></div>
-      <div class="box"><span>14</span><a href="category.php?category=design and development">design and development</a></div>
-      <div class="box"><span>15</span><a href="category.php?category=food and drinks">food and drinks</a></div>
-      <div class="box"><span>16</span><a href="category.php?category=lifestyle">lifestyle</a></div>
-      <div class="box"><span>17</span><a href="category.php?category=health and fitness">health and fitness</a></div>
-      <div class="box"><span>18</span><a href="category.php?category=business">business</a></div>
-      <div class="box"><span>19</span><a href="category.php?category=shopping">shopping</a></div>
-      <div class="box"><span>20</span><a href="category.php?category=animations">animations</a></div>
+      <div class="box"><span>01</span><a href="category.php?category=nature">Aberdeen</a></div>
+      <div class="box"><span>02</span><a href="category.php?category=eduction">Glasgow</a></div>
+      <div class="box"><span>03</span><a href="category.php?category=pets and animals">Edinburgh</a></div>
+      <div class="box"><span>04</span><a href="category.php?category=technology">Dundee</a></div>
    </div>
 
 </section>
