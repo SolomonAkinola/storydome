430c184439274839134ac88481526a4fdc8d5a36
SolomonAkinola <124267019+SolomonAkinola@users.noreply.github.com>
2023-04-04 01:05:53 +0100

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <div class="footer__socials">
+        <a href="https://youtube.com" target="_blank"><i class="uil uil-youtube"></i></a>
+        <a href="https://facebook.com" target="_blank"><i class="uil uil-facebook-f"></i></a>
+        <a href="https://instagram.com" target="_blank"><i class="uil uil-instagram-alt"></i></a>
+        <a href="https://linkedin.com" target="_blank"><i class="uil uil-linkedin"></i></a>
+        <a href="https://twitter.com" target="_blank"><i class="uil uil-twitter"></i></a>
+    </div>
+    <div class="container footer__container">
+        <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="">Aberdeen</a></li>
+                <li><a href="">Glasgow</a></li>
+                <li><a href="">Dundee</a></li>
+                <li><a href="">Edinburgh</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Support</h4>
+            <ul>
+                <li><a href="">Chat</a></li>
+                <li><a href="">Email</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Stories</h4>
+            <ul>
+                <li><a href="">Recent</a></li>
+                <li><a href="">Popular</a></li>
+                <li><a href="">Categories</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Permalinks</h4>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Stories</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </article>
+    </div>
+    <div class="footer__copyright">
+        <small>Copyright &copy; 2023 STORYDOME</small>
+    </div>
+</footer>
+
+
+<script src="<?= ROOT_URL ?>js/main.js"></script>
+</body>
+
+</html>
