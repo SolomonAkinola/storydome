a8db2720b97e2ae0f5bf4a27a62b362006b1fc32
Solomon Akinola <s.akinola@rgu.ac.uk>
2023-04-04 08:43:20 +0100

    contact update

--- contact.php
+++ contact.php
@@ -6,7 +6,8 @@ include 'partials/header.php';
 
 
 <section class="empty__page">
-    <h1>Contact Page</h1>
+    <h1>Contact Us</h1>
+	<p> Kindly send an email to s.akinola@rgu.ac.uk </p>
 </section>
 
 
